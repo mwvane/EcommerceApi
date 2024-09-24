822ea2dc2f3478b7daeb1d4815a02e58f42d686d
mwvane <bzishvili57@gmail.com>
2024-09-24 23:12:06 +0400

    Update

---
+++ Ecommerce.Core/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Core.Interfaces
+{
+    public interface ICategoryRepository: IRepository<Category>
+    {
+        Task<bool> IsCategoryNameExist(string name);
+    }
+}
